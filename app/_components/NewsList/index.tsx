d3dcc7ba0e4ce64f2d58f721eb5af3e6fa9a20ec
k1smz <142282543+k1smz@users.noreply.github.com>
2024-09-01 08:20:35 +0900

    3章まで完了

---
+++ app/_components/NewsList/index.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+import styles from "./index.module.css";
+import Category from "../Category";
+import Date from "../Date";
+import { News } from "@/app/_libs/microcms";
+
+type props = {
+  news: News[];
+};
+
+export default function NewsList({ news }: props) {
+  if (news.length === 0) {
+    return <div>記事がありません</div>;
+  }
+  return (
+    <ul>
+      {news.map((article) => (
+        <li key={article.id} className={styles.list}>
+          <div className={styles.link}>
+            <Image className={styles.image} src="/no-image.png" alt="no Image" width={1200} height={630} />
+            <dl className={styles.content}>
+              <dt className={styles.noesItemTitle}>{article.title}</dt>
+              <dd className={styles.meta}>
+                <Category category={article.category} />
+                <Date date={article.publishedAt ?? article.createdAt} />
+              </dd>
+            </dl>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
