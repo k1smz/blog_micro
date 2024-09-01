4ef9435ef41a465ba2519b69bba354606279d137
k1smz <142282543+k1smz@users.noreply.github.com>
2024-09-01 09:16:40 +0900

    ヘッダ・フッダー追加

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import styles from "./index.module.css";
 import Category from "../Category";
@@ -17,16 +18,18 @@ export default function NewsList({ news }: props) {
     <ul>
       {news.map((article) => (
         <li key={article.id} className={styles.list}>
-          <div className={styles.link}>
-            <Image className={styles.image} src="/no-image.png" alt="no Image" width={1200} height={630} />
-            <dl className={styles.content}>
-              <dt className={styles.noesItemTitle}>{article.title}</dt>
-              <dd className={styles.meta}>
-                <Category category={article.category} />
-                <Date date={article.publishedAt ?? article.createdAt} />
-              </dd>
-            </dl>
-          </div>
+          <Link href={`/news/${article.id}`} className={styles.link}>
+            <div className={styles.link}>
+              <Image className={styles.image} src="/no-image.png" alt="no Image" width={1200} height={630} />
+              <dl className={styles.content}>
+                <dt className={styles.noesItemTitle}>{article.title}</dt>
+                <dd className={styles.meta}>
+                  <Category category={article.category} />
+                  <Date date={article.publishedAt ?? article.createdAt} />
+                </dd>
+              </dl>
+            </div>
+          </Link>
         </li>
       ))}
     </ul>
